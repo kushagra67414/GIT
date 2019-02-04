a79aa0bcd029525b0dbc05f8d9892ccf74a5390f
kushagra67414 <500067414@stu.upes.ac.in>
2019-02-04 10:41:26 +0530

    first commit

---
+++ ADSlab1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is"<<c<<"\n";
+
+    return 0;
+}
